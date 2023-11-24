7df7e36a4d24dced0dab785c19cb2d35ac1163d2
Yiyan Wang <wang-yiyan@qq.com>
2023-11-24 13:59:12 +0800

    feat: 27. Remove Element.

---
+++ 27. Remove Element/Solution.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+using namespace std;
+
+// https://leetcode.com/problems/remove-element/
+
+class Solution
+{
+public:
+    int removeElement(vector<int> &nums, int val)
+    {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != val)
+            {
+                if (i != j)
+                {
+                    nums[j] = nums[i];
+                }
+                j++;
+            }
+        }
+        return j;
+    }
+};
