50b713dfbc8a9d542ce4c5943a008aff03a1544d
Yiyan Wang <wang-yiyan@qq.com>
2023-12-11 09:06:56 +0800

    feat: remove operator+= in zigzag conversion.

--- 6. Zigzag Conversion/math.cpp
+++ 6. Zigzag Conversion/math.cpp
@@ -14,16 +14,17 @@ public:
         int n = s.size();
         int round = (n + depth - 2) / (2 * depth) + 1;
 
-        string ans = "";
+        string ans = string(n, ' ');
+        int pos = 0;
         for (int i = 0; i <= depth; i++) {
             for (int j = 0; j < round; j++) {
                 int x1 = 2 * depth * j - i;
                 int x2 = 2 * depth * j + i;
-                if (x1 >= 0 && x1 < n && i != depth) {
-                    ans += s[x1];
+                if (i != depth && x1 >= 0 && x1 < n) {
+                    ans[pos++] = s[x1];
                 }
-                if (x2 < n && i != 0) {
-                    ans += s[x2];
+                if (i != 0 && x2 < n) {
+                    ans[pos++] = s[x2];
                 }
             }
         }
