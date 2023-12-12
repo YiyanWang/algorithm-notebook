f66edea71caa3db04d901793f92e4a92b9706078
Yiyan Wang <wang-yiyan@qq.com>
2023-12-12 22:30:21 +0800

    feat: 167. Two Sum II - Input Array Is Sorted.

---
+++ 167. Two Sum II - Input Array Is Sorted/two_pointers.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0, j = numbers.size() - 1;
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum > target) {
+                j--;
+            } else if (sum < target) {
+                i++;
+            } else {
+                break;
+            }
+        }
+
+        return { i + 1, j + 1 };
+    }
+};
