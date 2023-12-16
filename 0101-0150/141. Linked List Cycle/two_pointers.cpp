13063e4ee7eb44bf739274d11ed695ee5b45ed66
Yiyan Wang <wang-yiyan@qq.com>
2023-12-16 22:16:13 +0800

    feat: 141. Linked List Cycle.

---
+++ 0101-0150/141. Linked List Cycle/two_pointers.cpp
@@ -0,0 +1,31 @@
+#include <cstddef>
+using namespace std;
+
+// https://leetcode.com/problems/linked-list-cycle/
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if (!head) {
+            return false;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        do {
+            slow = slow->next;
+            fast = fast->next;
+            if (!fast || !fast->next) {
+                return false;
+            }
+            fast = fast->next;
+        } while (slow != fast);
+        return true;
+    }
+};
