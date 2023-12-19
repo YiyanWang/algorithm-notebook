8d31bf0eff71275082207007386eda60b94f70c5
Yiyan Wang <wang-yiyan@qq.com>
2023-12-20 00:03:27 +0800

    feat: 173. Binary Search Tree Iterator.

---
+++ 0151-0200/173. Binary Search Tree Iterator/depth_first_search.cpp
@@ -0,0 +1,46 @@
+#include <stack>
+using namespace std;
+
+// https://leetcode.com/problems/binary-search-tree-iterator/
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class BSTIterator {
+public:
+    stack<TreeNode *> s;
+    BSTIterator(TreeNode *root) {
+        while (root != NULL) {
+            s.push(root);
+            root = root->left;
+        }
+    }
+
+    int next() {
+        TreeNode *top = s.top();
+        s.pop();
+        TreeNode *right = top->right;
+        while (right != NULL) {
+            s.push(right);
+            right = right->left;
+        }
+        return top->val;
+    }
+
+    bool hasNext() {
+        return !s.empty();
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
