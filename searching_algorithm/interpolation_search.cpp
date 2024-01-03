da88f14151b62b282df15c76a7a10ace0f277eb0
Yiyan Wang <wang-yiyan@qq.com>
2024-01-03 21:49:07 +0800

    feat: searching algorithm - interpolation search.

---
+++ searching_algorithm/interpolation_search.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+using namespace std;
+
+int interpolationSearch(int arr[], int n, int target) {
+    int left = 0, right = n - 1;
+
+    while (left <= right && target >= arr[left] && target <= arr[right]) {
+        int mid = left + (target - arr[left]) * (right - left) / (arr[right] - arr[left]);
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] > target) right = mid - 1;
+        else left = mid + 1;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int target = 10;
+    int n = sizeof(arr) / sizeof(int);
+
+    int result = interpolationSearch(arr, n, target);
+    if (result == -1)
+        cout << "Element is not present in array" << endl;
+    else
+        cout << "Element is present at index " << result << endl;
+    return 0;
+}
